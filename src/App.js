f18aeb5e529d564f42717b463263ebd93d246c9d
Bekchanov Fayzulla Shexnazarovich <92919817+fayzullabekchanov@users.noreply.github.com>
2025-09-29 09:46:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 
-import 'bootstrap/dist/css/bootstrap.css';
-import './App.css';
-
-
-
-
-function App() {
-    return (
-        <div className="App">
-            <h1>Faqat bitta variantini tanlang</h1>
-
-
-
-             </div>
-
-    );
+export default function App() {
+useEffect(() => {
+if ('paintWorklet' in CSS) {
+CSS.paintWorklet.addModule('https://unpkg.com/smooth-corners');
 }
+}, []);
 
-export default App;
+
+return (
+<main className="flex gap-8 min-h-screen items-center justify-center bg-[hsl(240deg_17.5%_90%)]">
+<button className="squircle">Get Started</button>
+<button className="radius">Get Started</button>
+</main>
+);
+}
