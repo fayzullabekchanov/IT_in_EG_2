f6604e3074c9316edaa73e73c0b1d2ebd88d42b3
Bekchanov Fayzulla Shexnazarovich <92919817+fayzullabekchanov@users.noreply.github.com>
2025-09-29 09:49:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function App() {
   return (
     <main className="flex gap-8 min-h-screen items-center justify-center bg-[hsl(240deg_17.5%_90%)]">
-      <button className="btn-squircle">Get Started</button>
-      <button className="btn-radius">Get Started</button>
+      <button className="btn-squircle">Computer 1</button>
+      <button className="btn-radius">INFORMATION TECHNOLOGY IN ELEMENTARY GRADES 2</button>
     </main>
   );
 }
