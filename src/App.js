cb548090e96a14aa5fd2def4ff24d78175a071a2
Bekchanov Fayzulla Shexnazarovich <92919817+fayzullabekchanov@users.noreply.github.com>
2025-09-29 09:47:47 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-import React, { useEffect } from 'react';
-
+import React from "react";
 
 export default function App() {
-useEffect(() => {
-if ('paintWorklet' in CSS) {
-CSS.paintWorklet.addModule('https://unpkg.com/smooth-corners');
-}
-}, []);
-
-
-return (
-<main className="flex gap-8 min-h-screen items-center justify-center bg-[hsl(240deg_17.5%_90%)]">
-<button className="squircle">Get Started</button>
-<button className="radius">Get Started</button>
-</main>
-);
+  return (
+    <main className="flex gap-8 min-h-screen items-center justify-center bg-[hsl(240deg_17.5%_90%)]">
+      <button className="btn-squircle">Get Started</button>
+      <button className="btn-radius">Get Started</button>
+    </main>
+  );
 }
